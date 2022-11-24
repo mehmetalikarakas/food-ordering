8ded74bd6dead6db0b9375591e8c2de6ab0612a9
mehmetalikarakas <mali.karakas11@gmail.com>
2022-11-24 09:53:19 +0300

    update

---
+++ components/layout/Header.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Logo from "./ui/Logo";
+import { FaUserAlt, FaSearch } from "react-icons/fa";
+import { HiShoppingCart } from "react-icons/hi";
+
+const Header = () => {
+  const [search, setSearch] = useState(true);
+
+  return (
+    <div className="h-[5.5rem] bg-secondary ">
+      <div className="container mx-auto text-white flex justify-between items-center h-full">
+        <div>
+          <Logo />
+        </div>
+
+        <nav>
+          <ul className="flex gap-x-2">
+            <li className="px-[0.313rem] py-[1.25rem] hover:text-primary">
+              <a href="" className="uppercase">
+                Home
+              </a>
+            </li>
+            <li className="px-[0.313rem] py-[1.25rem] hover:text-primary">
+              <a href="" className="uppercase">
+                Menu
+              </a>
+            </li>
+            <li className="px-[0.313rem] py-[1.25rem] hover:text-primary">
+              <a href="" className="uppercase">
+                About
+              </a>
+            </li>
+            <li className="px-[0.313rem] py-[1.25rem] hover:text-primary">
+              <a href="" className="uppercase">
+                Book Table
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="flex gap-x-4 items-center">
+          <a href="#">
+            <FaUserAlt className="hover:text-primary transition-all" />
+          </a>
+          <a href="#">
+            <HiShoppingCart className="hover:text-primary transition-all" />
+          </a>
+          <button onClick={() => setSearch(false)}>
+            <FaSearch className="hover:text-primary transition-all" />
+          </button>
+          <a href="#">
+            <button className="btn-primary">Order Online</button>
+          </a>
+        </div>
+      </div>
+      {search && <div className="text-9xl">Modal</div>}
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/home/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div className="font-dancing text-center text-3xl">Home Pages</div>;
+};
+
+export default index;
