8c43f0827724c7b29005045c61e7da3af16405bd
mehmetalikarakas <mali.karakas11@gmail.com>
2022-12-13 20:21:09 +0300

    update

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import Logo from "../ui/Logo";
 import { FaUserAlt, FaSearch } from "react-icons/fa";
 import { HiShoppingCart } from "react-icons/hi";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Search from "../ui/Search";
 
 const Header = () => {
   const [search, setSearch] = useState(false);
+  const [isMenuModal, setIsMenuModal] = useState(false);
 
   return (
     <div className="h-[5.5rem] bg-secondary ">
@@ -14,8 +16,11 @@ const Header = () => {
           <Logo />
         </div>
 
-        <nav>
-          <ul className="flex gap-x-2">
+        <nav
+          className={`sm:static absolute top-0 left-0 grid place-content-center w-full h-full sm:w-auto sm:h-auto sm:text-white text-black sm:bg-transparent bg-white
+        ${isMenuModal !== true && "hidden"}  `}
+        >
+          <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
             <li className="px-[0.313rem] py-[1.25rem] hover:text-primary">
               <a href="" className="uppercase">
                 Home
@@ -49,9 +54,12 @@ const Header = () => {
           <button onClick={() => setSearch(true)}>
             <FaSearch className="hover:text-primary transition-all" />
           </button>
-          <a href="#">
+          <a href="#" className="md:inline-block hidden">
             <button className="btn-primary">Order Online</button>
           </a>
+          <button onClick={() => setIsMenuModal(true)}>
+            <GiHamburgerMenu className="text-2xl hover:text-primary transition-all md:hidden inline-block" />
+          </button>
         </div>
       </div>
       {search && <Search setSearch={setSearch} />}
--- components/ui/Search.jsx
+++ components/ui/Search.jsx
@@ -1,14 +1,66 @@
+import Image from "next/legacy/image";
 import React from "react";
 import OutsideClickHandler from "react-outside-click-handler";
 import Title from "./Title";
+import { GiCancel } from "react-icons/gi";
 
 const Search = ({ setSearch }) => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen z-50 after:content-[''] after:w-screen after:h-screen after:bg-red-600 after:absolute after:top-0 after:left-0 opacity-50 first-of-type:h-screen grid place-content-center">
+    <div className="fixed top-0 left-0 w-screen h-screen z-50 after:content-[''] after:w-screen after:h-screen after:bg-white after:absolute after:top-0 after:opacity-50 after:left-0 first-of-type:h-screen grid place-content-center">
       <OutsideClickHandler onOutsideClick={() => setSearch(false)}>
         <div className="w-full h-full grid place-content-center ">
-          <div className="relative z-50 w-[600px] h-[600px] bg-white border p-5">
+          <div className="relative z-50 md:w-[600px] w-[370px] border-2 border-primary p-10 rounded-3xl">
             <Title addClass="text-[40px] text-center">Search</Title>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="border w-full my-10"
+            />
+
+            <ul className="mt-10">
+              <li className="p-2 hover:bg-primary transition-all">
+                <div className="flex items-center justify-between">
+                  <Image
+                    src="/images/f1.png"
+                    alt="food"
+                    width={48}
+                    height={48}
+                  />
+                  <span className="font-bold">Good Pizza</span>
+                  <span className="font-bold">$10</span>
+                </div>
+              </li>
+              <li className="p-2 hover:bg-primary transition-all">
+                <div className="flex items-center justify-between">
+                  <Image
+                    src="/images/f1.png"
+                    alt="food"
+                    width={48}
+                    height={48}
+                  />
+                  <span className="font-bold">Good Pizza</span>
+                  <span className="font-bold">$10</span>
+                </div>
+              </li>
+              <li className="p-2 hover:bg-primary transition-all">
+                <div className="flex items-center justify-between">
+                  <Image
+                    src="/images/f1.png"
+                    alt="food"
+                    width={48}
+                    height={48}
+                  />
+                  <span className="font-bold">Good Pizza</span>
+                  <span className="font-bold">$10</span>
+                </div>
+              </li>
+            </ul>
+            <button
+              className="absolute top-10 right-10"
+              onClick={() => setSearch(false)}
+            >
+              <GiCancel size={30} className="transition-all" />
+            </button>
           </div>
         </div>
       </OutsideClickHandler>
